ee52eba0b811f2aad9a33e2e647208e4334d81b0
Kristina Romanova <hrusha@freenet.de>
2023-07-25 10:12:42 +0200

    getDescription

--- script.js
+++ script.js
@@ -45,26 +45,26 @@ async function geoLocationWeather(){
         case  3: return "Overcast";
         case 45: return "Fog";
         case 48: return "Depositing rime fog";
-        case 51: return "Drizzle: Light";
+        case 51: return "Drizzle: light";
         case 53: return "Drizzle:moderate";
         case 55: return "Dense intensity";
-        case 56: return "Freezing Drizzle: Light";
-        case 57: return "Freezing Drizzle: dense intensity";
-        case 61: return "Rain: Slight";
+        case 56: return "Freezing drizzle: light";
+        case 57: return "Freezing drizzle: dense intensity";
+        case 61: return "Rain: slight";
         case 63: return "Rain: moderate";
         case 65: return "Rain: heavy intensity";
-        case 66: return "Freezing Rain: Light";
-        case 67: return "Freezing Rain: heavy intensity";
-        case 71: return "Snow fall: Slight";
+        case 66: return "Freezing rain: Light";
+        case 67: return "Freezing rain: heavy intensity";
+        case 71: return "Snow fall: slight";
         case 73: return "Snow fall: moderate";
         case 75: return "Snow fall: heavy intensity";
         case 77: return "Snow grains";
-        case 80: return "Rain showers:Rain showers: Slight";
-        case 81: return "Rain showers: moderate, and violent";
+        case 80: return "Rain showers: slight";
+        case 81: return "Rain showers: moderate";
         case 82: return "Rain showers: violent"
         case 85: return "Snow showers slight"
         case 86: return "Snow showers heavy";
-        case 95: return "Thunderstorm: Slight or moderate";
+        case 95: return "Thunderstorm: slight or moderate";
         case 96: return "Thunderstorm with slight"
         case 99: return "Thunderstorm with heavy hail"
         default: return " - ";
@@ -163,6 +163,7 @@ async function geoLocationWeather(){
     
 
   geoLocationWeather();
+  
 // 1. функция, которая интерпертирует код погоды
   //  0 -> "clear sky"
   //  1 -> "Mainly clear"
